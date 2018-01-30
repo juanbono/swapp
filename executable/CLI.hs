6e9ae5609179631a1cc58ecb082f64261319982d
Juan Bono <juanbono94@gmail.com>
2018-01-30 14:46:12 -0300

    #1 add cli module

--- executable/CLI.hs
+++ executable/CLI.hs
@@ -1,3 +1,28 @@
 {- CLI -}
 
-module CLI where
+module CLI (parseCharacters, InputChars(..)) where
+
+import Options.Applicative
+import Data.Monoid
+
+parseCharacters :: IO InputChars
+parseCharacters = execParser opts
+
+data InputChars = InputChars
+  { firstCharacter  :: String
+  , secondCharacter :: String
+  } deriving (Show, Eq)
+
+parseInputChars :: Parser InputChars
+parseInputChars = InputChars
+  <$> strOption
+  (help "First character" <> metavar "FIRST" <> short 'f')
+  <*> strOption
+  (help "Second character" <> metavar "SECOND" <> short 's')
+
+opts :: ParserInfo InputChars
+opts = info (parseInputChars <**> helper) (fullDesc <> description <> hd)
+  where
+    description = progDesc "Simple CLI App that gives you the set of films in common between two Star Wars characters"
+    hd = header "SWAPP"
+
--- executable/Main.hs
+++ executable/Main.hs
@@ -1,7 +1,10 @@
--- It is generally a good idea to keep all your business logic in your library
--- and only use it in the executable. Doing so allows others to use what you
--- wrote in their libraries.
-import qualified Types
+
+import CLI
 
 main :: IO ()
-main = Types.main
+main = do
+  InputChars first second <- parseCharacters -- get the input from command line
+
+  print $ "First Character: " ++ first
+  print $ "Second Character: " ++ second
+
